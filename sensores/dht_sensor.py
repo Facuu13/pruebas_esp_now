a328332cdb3c011c1bf5eb192e74ad6094ccc092
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2024-06-15 11:15:57 -0300

    cambios de organizacion

--- sensores/dht_sensor.py
+++ sensores/dht_sensor.py
@@ -9,41 +9,29 @@ import dht
 import machine
 import struct
 
-# org.bluetooth.service.environmental_sensing
+# UUIDs
 _ENV_SENSE_UUID = bluetooth.UUID(0x181A)
-# org.bluetooth.characteristic.temperature
 _ENV_SENSE_TEMP_UUID = bluetooth.UUID(0x2A6E)
-# org.bluetooth.characteristic.humidity
 _ENV_SENSE_HUM_UUID = bluetooth.UUID(0x2A6F)
-# org.bluetooth.characteristic.gap.appearance.xml
 _ADV_APPEARANCE_GENERIC_THERMOMETER = const(768)
-
-# How frequently to send advertising beacons.
 _ADV_INTERVAL_MS = 250_000
 
 # Setup DHT11 sensor
-dht_pin = machine.Pin(2)  # Adjust pin number according to your setup
+dht_pin = machine.Pin(32)
 dht_sensor = dht.DHT11(dht_pin)
 
 # Register GATT server.
 temp_service = aioble.Service(_ENV_SENSE_UUID)
-temp_characteristic = aioble.Characteristic(
-    temp_service, _ENV_SENSE_TEMP_UUID, read=True, notify=True
-)
-hum_characteristic = aioble.Characteristic(
-    temp_service, _ENV_SENSE_HUM_UUID, read=True, notify=True
-)
+temp_characteristic = aioble.Characteristic(temp_service, _ENV_SENSE_TEMP_UUID, read=True, notify=True)
+hum_characteristic = aioble.Characteristic(temp_service, _ENV_SENSE_HUM_UUID, read=True, notify=True)
 aioble.register_services(temp_service)
 
-# Helper to encode the temperature characteristic encoding (sint16, hundredths of a degree).
 def _encode_temperature(temp_deg_c):
     return struct.pack("<h", int(temp_deg_c * 100))
 
-# Helper to encode the humidity characteristic encoding (sint16, hundredths of a percent).
 def _encode_humidity(hum_percent):
     return struct.pack("<h", int(hum_percent * 100))
 
-# This would be periodically polling a hardware sensor.
 async def sensor_task():
     while True:
         try:
@@ -57,22 +45,21 @@ async def sensor_task():
             print("Failed to read from DHT sensor:", e)
         await asyncio.sleep_ms(1000)
 
-# Serially wait for connections. Don't advertise while a central is connected.
-async def peripheral_task():
+async def peripheral_task(sensor_id):
     while True:
         async with await aioble.advertise(
             _ADV_INTERVAL_MS,
-            name="mpy-temp",
+            name=f"DHT11-{sensor_id}",
             services=[_ENV_SENSE_UUID],
             appearance=_ADV_APPEARANCE_GENERIC_THERMOMETER,
         ) as connection:
             print("Connection from", connection.device)
             await connection.disconnected()
 
-# Run both tasks.
 async def main():
+    sensor_id = 1  # Change this ID for each sensor
     t1 = asyncio.create_task(sensor_task())
-    t2 = asyncio.create_task(peripheral_task())
+    t2 = asyncio.create_task(peripheral_task(sensor_id))
     await asyncio.gather(t1, t2)
 
 asyncio.run(main())
