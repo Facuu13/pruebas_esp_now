ebab079de7fd8cf58a898bfed7d5048e8bf7a46b
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2024-06-16 10:38:32 -0300

    comentarios

--- receive_data_espnow.py
+++ receive_data_espnow.py
@@ -4,17 +4,18 @@ import espnow
 # A WLAN interface must be active to send()/recv()
 sta = network.WLAN(network.STA_IF)
 sta.active(True)
-sta.disconnect()   # Because ESP8266 auto-connects to last Access Point
+sta.disconnect()   
 
 e = espnow.ESPNow()
 e.active(True)
 
 #callback
 def recv_cb(e):
-    while True:  # Read out all messages waiting in the buffer
-        mac, msg = e.irecv(0)  # Don't wait if no messages left
+    while True:  # Leer todos los mensajes que esperan en el búfer
+        mac, msg = e.irecv(0)  # No esperar si no hay mensajes
         if mac is None:
             return
         print(mac, msg)
-        
+
+# Asignar la función de callback a ESP-NOW        
 e.irq(recv_cb)
