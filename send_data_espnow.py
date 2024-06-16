472761b35c2dbc97b7171a1169b6fd508dafa3d7
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2024-06-16 10:43:22 -0300

    info del deep_sleep

--- send_data_espnow.py
+++ send_data_espnow.py
@@ -1,5 +1,7 @@
 import network
 import espnow
+import time
+import machine
 
 # A WLAN interface must be active to send()/recv()
 sta = network.WLAN(network.STA_IF)  # Or network.AP_IF
@@ -13,8 +15,13 @@ e.add_peer(peer)      # Must add_peer() before send()
 
 e.send(peer, "Starting...")
 for i in range(100):
-    e.send(peer, str(i)*20, True)
-
+    e.send(peer, str(i), True)
+    time.sleep(0.2)
+    
 e.send(peer, b'end')
 
-
+# Configurar el ESP para despertar en 60 segundos y entrar en sueño profundo
+#print("Entrando en modo de sueño profundo por 60 segundos...")
+#machine.deepsleep(6000)
+#debemos poner esta script en el boot para que se ejecute automaticamente en cada inicio
+#porque el sueño profundo hace que se reinicie la placa
