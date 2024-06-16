d85981d8eb7d9024423e00cbf3367794577ae5e9
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2024-06-16 13:34:50 -0300

    comentarios

--- esp-now/wifi_espnow_master.py
+++ esp-now/wifi_espnow_master.py
@@ -2,35 +2,36 @@ import network
 import espnow
 import time
 
-ssid = "quepasapatejode"
-password = "losvilla08"
+ssid = "quepasapatejode"   # Nombre de la red WiFi 
+password = "losvilla08"    # Contraseña de la red WiFi
 
-def wifi_reset():   # Reset wifi to AP_IF off, STA_IF on and disconnected
+def wifi_reset():
     sta = network.WLAN(network.STA_IF); sta.active(False)
     ap = network.WLAN(network.AP_IF); ap.active(False)
-    sta.active(True)
-    while not sta.active():
+    sta.active(True)    # Activar el modo estación
+    while not sta.active():   
         time.sleep(0.1)
-    sta.disconnect()   # For ESP8266
-    while sta.isconnected():
+    sta.disconnect()   
+    while sta.isconnected():   # Esperar hasta que se desconecte de cualquier red WiFi anterior
         time.sleep(0.1)
-    return sta, ap
+    return sta, ap   # Devolver los objetos de estación y punto de acceso
 
-#callback
+# Función de callback para recibir datos
 def recv_cb(e):
-    while True:  # Leer todos los mensajes que esperan en el búfer
-        mac, msg = e.irecv(0)  # No esperar si no hay mensajes
-        if mac is None:
+    while True:    # Leer todos los mensajes que esperan en el búfer
+        mac, msg = e.irecv(0)   # No esperar si no hay mensajes
+        if mac is None:   # Si no hay dirección MAC, salir del bucle
             return
-        print(mac, msg)
+        print(mac, msg)   # Imprimir la dirección MAC y el mensaje recibido
 
-sta, ap = wifi_reset()  # Reset wifi to AP off, STA on and disconnected
+sta, ap = wifi_reset()
 sta.connect(ssid, password)
-while not sta.isconnected():  # Wait until connected...
+while not sta.isconnected():
     time.sleep(0.1)
     
-sta.config(pm=sta.PM_NONE)  # ..then disable power saving
-print("Proxy running on channel:", sta.config("channel"))
+sta.config(pm=sta.PM_NONE)   # Deshabilitar el ahorro de energía después de la conexión
+print("Proxy corriendo en el canal:", sta.config("channel"))   # Imprimir el canal en el que se está ejecutando
 e = espnow.ESPNow(); e.active(True)
-# Asignar la función de callback a ESP-NOW        
-e.irq(recv_cb)
+
+# Asignar la función de callback a ESP-NOW
+e.irq(recv_cb)   # Establecer la función recv_cb como la función de callback para manejar los datos recibidos
