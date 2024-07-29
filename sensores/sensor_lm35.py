34b38826b1d2ec9aa7ba8a31329673d70a3317e4
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2024-07-29 17:22:29 -0300

    se agrego la funcion para que leamos el sensor lm35 y podamos enviar el dato

--- sensores/sensor_lm35.py
+++ sensores/sensor_lm35.py
@@ -1,22 +1,35 @@
 import time
-import machine
+from machine import ADC, Pin
 import json
 from network_espnow import SensorBase
 
 class SensorLM35(SensorBase):
     def __init__(self):
         super().__init__()
+        # Configurar el pin GPIO34 como entrada analógica
+        self.adc = ADC(Pin(34))
+        self.adc.atten(ADC.ATTN_11DB)  # Configurar el rango de atenuación (0-3.3V)
+        self.adc.width(ADC.WIDTH_12BIT)  # Configurar la resolución (12 bits: 0-4095)
 
+    def read_temperature(self):
+        # Leer el valor del ADC (0-4095)
+        adc_value = self.adc.read()
+        # Convertir el valor del ADC a voltaje (0-3.3V)
+        voltage = adc_value / 4095.0 * 3.3
+        # Convertir el voltaje a temperatura (Celsius)
+        temperature = voltage * 100
+        return round(temperature, 2)  # Redondeamos a 2 decimales
+    
     def send_sensor_data(self):
-        for i in range(100):
-            data = {
+        temp = self.read_temperature()
+        data = {
                 "topic": "sensor/temp",
-                "value": i
+                "value": temp
             }
-            data_str = json.dumps(data)
-            self.e.send(self.peer_mac, data_str)
-            print("Mensaje enviado:", data_str)
-            time.sleep(2)
+        data_str = json.dumps(data)
+        self.e.send(self.peer_mac, data_str)
+        print("Mensaje enviado:", data_str)
+        time.sleep(2)
 
 # from sensor_lm35 import SensorLM35
 
