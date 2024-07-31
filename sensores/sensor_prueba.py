b101abc7de1fca348f59db1303a699b2ee07dec3
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2024-07-31 20:38:12 -0300

    tenemos un script de prueba

---
+++ sensores/sensor_prueba.py
@@ -0,0 +1,23 @@
+import time
+import machine
+from network_espnow import SensorBase
+import json
+
+class SensorPrueba(SensorBase):
+    def __init__(self):
+        super().__init__()
+
+    def send_sensor_data(self):
+        for i in range(100):
+            data = {
+                    "topic": "sensor/prueba",
+                    "value": i
+                }
+            data_str = json.dumps(data)
+            self.e.send(self.peer_mac, data_str)
+            print("Mensaje enviado:", data_str)
+            time.sleep(1)
+            
+# from sensor_prueba import SensorPrueba
+# sensor = SensorPrueba()
+# sensor.send_sensor_data()
