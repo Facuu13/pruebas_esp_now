278a12441c1510ce810495ccfb7a4fc80df20086
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2024-07-31 19:44:08 -0300

    el dht11 no se utiliza por lo que lo eliminamos su script

--- sensores/sensor_bme280.py
+++ sensores/sensor_bme280.py
@@ -3,8 +3,8 @@ import machine
 from network_espnow import SensorBase
 
 class SensorBME280(SensorBase):
-    def __init__(self, peer_mac):
-        super().__init__(peer_mac)
+    def __init__(self):
+        super().__init__()
 
     def send_sensor_data(self):
         for i in range(100):
