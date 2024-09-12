43515bcfa6758d38517a524fecd943892a54a13a
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2024-09-12 15:16:10 -0300

    modificaciones en el frontend

--- master/frontend/script.js
+++ master/frontend/script.js
@@ -36,18 +36,21 @@ function cargarDatos() {
             return response.text();
         })
         .then(data => {
-            const sensorDataContainer = document.getElementById('sensor-data');
-            sensorDataContainer.innerHTML = ''; // Limpiar contenido previo
-            
-            const sensors = data.trim().split("\n\n"); // Separar cada conjunto de datos
+          const sensorDataContainer = document.getElementById("sensor-data");
+          sensorDataContainer.innerHTML = ""; // Limpiar contenido previo
 
-            sensors.forEach(sensor => {
-                const [mac, topic, value] = sensor.split("\n");
-                let isChecked = value.includes("True") ? "checked" : "";
+          const releDataContainer = document.getElementById("rele-data");
+          releDataContainer.innerHTML = ""; // Limpiar contenido previo
 
-                // Crear switch para sensor/rele/state
-                if (topic.includes("sensor/rele/state")) {
-                    const card = `
+          const sensors = data.trim().split("\n\n"); // Separar cada conjunto de datos
+
+          sensors.forEach((sensor) => {
+            const [mac, topic, value] = sensor.split("\n");
+            let isChecked = value.includes("True") ? "checked" : "";
+
+            // Crear switch para sensor/rele/state
+            if (topic.includes("sensor/rele/state")) {
+              const card = `
                         <div class="sensor-card">
                             <p><strong>Sensor: ${mac.split("/")[0]}</strong></p>
                             <p>Topic: ${topic}</p>
@@ -56,18 +59,18 @@ function cargarDatos() {
                                 <span class="slider round"></span>
                             </label>
                         </div>`;
-                    sensorDataContainer.innerHTML += card;
-                } else {
-                    // Para otros tipos de sensores, simplemente mostrar el valor
-                    const card = `
+              releDataContainer.innerHTML += card;
+            } else {
+              // Para otros tipos de sensores, simplemente mostrar el valor
+              const card = `
                         <div class="sensor-card">
                             <p><strong>Sensor: ${mac.split("/")[0]}</strong></p>
                             <p>Topic: ${topic}</p>
                             <p><span class="sensor-value">${value}</span></p>
                         </div>`;
-                    sensorDataContainer.innerHTML += card;
-                }
-            });
+              sensorDataContainer.innerHTML += card;
+            }
+          });
         })
         .catch(error => console.error('Error al cargar los datos:', error));
 }
