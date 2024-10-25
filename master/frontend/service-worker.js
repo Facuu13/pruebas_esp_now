52e5fa3f8e6a046d19765e2581b8ee9f0b5e8dda
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2024-10-25 19:39:35 -0300

    primera parte de la pwa

---
+++ master/frontend/service-worker.js
@@ -0,0 +1,44 @@
+// frontend/service-worker.js
+
+const CACHE_NAME = "invernadero-cache-v1";
+const urlsToCache = [
+  "frontend/index.html",
+  "frontend/script.js",
+  "frontend/styles.css",
+  "frontend/config.html",
+  "frontend/config.js",
+  "frontend/config.css",
+  "frontend/login.html",
+  "frontend/login.js",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
